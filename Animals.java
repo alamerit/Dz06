c73e5044efb57fdf41a8b948af2816e1ee7a3b6d
alamerit <39819079+alamerit@users.noreply.github.com>
2018-06-08 15:07:43 +0700

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,18 @@
+package Geekbrains.lesson_5.homework;
+
+ class Animals {
+
+   protected boolean loss ;
+   private String name;
+   private int swim;
+   private int run;
+    private float jump;
+    public Animals (String name, int swim, int run,float jump) {
+        this.name = name;
+        this.swim =  swim;
+        this.run = run;
+        this.jump = jump;
+    }
+   }
+
+
